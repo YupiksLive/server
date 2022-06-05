ab30949fefd752cbb1f754b26a3248941f9bf7fa
YupiksLive <svs731g@gmail.com>
2022-06-05 22:17:00 +0300

    Reformat code

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -9,16 +9,16 @@ public class Client {
     public static void main(String[] args) {
         String host = "localhost";
         int port = 8080;
-        try (Socket clientSocket = new Socket(host,port);
-             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))){
+        try (Socket clientSocket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
             out.println("GET / HTTP/1.1.\n" +
                     "Host: netology.ru\n\n\n");
             String resp = in.readLine();
             System.out.println(resp);
             InetAddress inetAddress = InetAddress.getByName(host);
-            System.out.println(host+", ip address: " +inetAddress.getHostAddress());
-        } catch (IOException ex){
+            System.out.println(host + ", ip address: " + inetAddress.getHostAddress());
+        } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
     }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -4,8 +4,9 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+
 public class Server {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         int port = 8080;
         while (true) {
             try (ServerSocket serverSocket = new ServerSocket(port);
